6d5355d9a349f255c1a1991815309790bbaa9f44
faysalsaade <faysalsaadeh@gmail.com>
2024-06-29 20:25:01 +0300

    create a new project

--- from_33_to37/index5.js
+++ from_33_to37/index5.js
@@ -14,21 +14,25 @@
 //   price -= 50;
 // }
 // console.log(price);
-let thename = "ahmed";
-let thegender = "male";
-let theage = "30";
-thegender === "male" ? console.log("mr") : console.log("mrs");
-let result = thegender === "male" ? "mr" : "mrs";
-document.write(result);
-console.log(
-  `hello ${
-    thegender === "male" ? console.log("mr") : console.log("mrs")
-  }  ${thename}`
-);
-theage < 20
-  ? console.log(20)
-  : theage > 20 && theage < 60
-  ? console.log("20 to 60")
-  : theage > 60
-  ? console.log("larger than 60")
-  : console.log("unknown");
+
+// let thename = "ahmed";
+// let thegender = "male";
+// let theage = "30";
+// thegender === "male" ? console.log("mr") : console.log("mrs");
+// let result = thegender === "male" ? "mr" : "mrs";
+// document.write(result);
+// console.log(
+//   `hello ${
+//     thegender === "male" ? console.log("mr") : console.log("mrs")
+//   }  ${thename}`
+// );
+// theage < 20
+//   ? console.log(20)
+//   : theage > 20 && theage < 60
+//   ? console.log("20 to 60")
+//   : theage > 60
+//   ? console.log("larger than 60")
+//   : console.log("unknown");
+let price = null;
+console.log(`the price is ${price || 200}`);
+console.log(`the price is ${price ?? 600}`);
