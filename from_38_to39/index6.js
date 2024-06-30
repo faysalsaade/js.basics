8964e2a06b5465864be7617a6987d0d86229aac4
faysalsaade <faysalsaadeh@gmail.com>
2024-06-30 12:23:50 +0300

    create a new project

---
+++ from_38_to39/index6.js
@@ -0,0 +1,16 @@
+let day = 0;
+
+switch (day) {
+  case 0:
+    console.log("saturday");
+    break;
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+  case 3:
+    console.log("sunday");
+    break;
+  default:
+    console.log("UNKONWNDAY");
+}
