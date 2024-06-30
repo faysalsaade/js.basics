f5357ea4db0fad2ced2d9dfaa19c4fde09c2d847
faysalsaade <faysalsaadeh@gmail.com>
2024-06-30 17:14:14 +0300

    create a new project

--- from_38_to39/index6.js
+++ from_38_to39/index6.js
@@ -1,15 +1,44 @@
-let day = 0;
+// let day = 0;
 
-switch (day) {
-  case 0:
-    console.log("saturday");
+// switch (day) {
+//   case 0:
+//     console.log("saturday");
+//   break;
+//   case 1:
+//     console.log("sunday");
+//     break;
+//   case 2:
+//   case 3:
+//     console.log("sunday");
+//     break;
+//   default:
+//     console.log("UNKONWNDAY");
+// }
+
+let day = "   monday  ";
+let fifi = day.trim();
+let fifi1 = fifi[0].toUpperCase() + fifi.slice(1);
+
+// You Need To Remove Spaces And Make First Letter Capital => Friday
+
+switch (fifi1) {
+  case "Sunday":
+  case "Friday":
+  case "Saturday":
+    console.log("No Appointments Available");
     break;
-  case 1:
-    console.log("sunday");
+  case "Monday":
+  case "Thursday":
+    console.log("From 10:00 AM To 5:00 PM");
     break;
-  case 2:
-  case 3:
-    console.log("sunday");
+  case Tuesday:
+    console.log("From 10:00 AM To 6:00 PM");
+    break;
+  case Wednesday:
+    console.log("From 10:00 AM To 7:00 PM");
+    break;
+  case World:
+    console.log("Its Not A Valid Day");
     break;
   default:
     console.log("UNKONWNDAY");
