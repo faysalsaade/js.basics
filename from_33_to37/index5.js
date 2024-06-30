bb7c33aac839aa45ca19fc28b438c79095cf4150
faysalsaade <faysalsaadeh@gmail.com>
2024-06-30 11:28:35 +0300

    create a new project

--- from_33_to37/index5.js
+++ from_33_to37/index5.js
@@ -1,38 +1,124 @@
-// let price = 100;
-// let discount = false;
-// let discountamount = 30;
-// let country = "lebanon";
-// if (discount === true) {
-//   price -= discountamount;
-// } else if (country === "spain") {
-//   if (student === true) {
-//     price -= discountamount + 30;
-//   } else {
-//     price -= 10;
-//   }
-// } else {
-//   price -= 50;
-// }
-// console.log(price);
+// // Test Case 1
+// let num = 9; // "009"
 
-// let thename = "ahmed";
-// let thegender = "male";
-// let theage = "30";
-// thegender === "male" ? console.log("mr") : console.log("mrs");
-// let result = thegender === "male" ? "mr" : "mrs";
-// document.write(result);
-// console.log(
-//   `hello ${
-//     thegender === "male" ? console.log("mr") : console.log("mrs")
-//   }  ${thename}`
-// );
-// theage < 20
-//   ? console.log(20)
-//   : theage > 20 && theage < 60
-//   ? console.log("20 to 60")
-//   : theage > 60
-//   ? console.log("larger than 60")
-//   : console.log("unknown");
-let price = null;
-console.log(`the price is ${price || 200}`);
-console.log(`the price is ${price ?? 600}`);
+// // Test Case 2
+// let num1 = 20; // "020"
+
+// // Test Case 3
+// let num2 = 110; // "110"
+
+// if (num < 10) {
+//   console.log(`00${num}`);
+// }
+// if (num1 > 10 && num1 < 100) {
+//   console.log(`0${num1}`);
+// }
+// if (num2 >= 100) {
+//   console.log(num2);
+// }
+
+// let num1 = 9;
+// let str = "9";
+// let str2 = "20";
+
+// if (num1 == str) {
+//   console.log("True");
+// }
+
+// if (num1 !== str) {
+//   console.log("True");
+// }
+
+// if (num1 !== str2) {
+//   console.log("True");
+// }
+
+// if (num1 != str2 && num1 !== str2) {
+//   console.log("True");
+// }
+
+// if (str != str2 && typeof str === typeof str2) {
+//   console.log("True");
+// }
+
+// let num1 = 10;
+// let num2 = 30;
+// let num3 = "30";
+// if (num3 > num1 && typeof num2 !== typeof num3) {
+//   console.log("True");
+// }
+// if (num3 > num1 && num2 == num3 && typeof num2 !== typeof num3) {
+//   console.log("True");
+// }
+// if (num3 != num1 && num3 !== num1 && typeof num2 !== typeof num3) {
+//   console.log("True");
+// }
+
+// Edit What You Want Here
+
+// let num1 = 10;
+// let num2 = 9;
+// let num3 = 10;
+// let num4 = 32;
+
+// /*
+//   Do Not Edit Below This Line
+//   Needed Output
+//   True 7 Times
+// */
+
+// // // Condition 1
+
+// if (num1 > num2) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // Condition 2
+
+// if (num1 > num2 && num1 < num4) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // // Condition 3
+
+// if (num1 > num2 && num1 === num3) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // // Condition 4
+
+// if (num1 + num2 < num4) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // // Condition 5
+
+// if (num1 + num3 < num4) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // // Condition 6
+
+// if (num1 + num2 + num3 < num4) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// // // Condition 7
+
+// if (num4 - (num1 + num3) + num2 === 21) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
