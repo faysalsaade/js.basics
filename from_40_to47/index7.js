2f42ef590c0ae76a41becd52e52678ce0f9a3784
faysalsaade <faysalsaadeh@gmail.com>
2024-07-01 20:27:07 +0300

    create a new project

--- from_40_to47/index7.js
+++ from_40_to47/index7.js
@@ -1,14 +1,19 @@
-let myfriends = ["Ahmed", "Mohamed", "Sayed", ["marwan", "ali"]];
+// let myfriends = ["Ahmed", "Mohamed", "Sayed", ["marwan", "ali"]];
 
-console.log(`hello ${myfriends[0]}`);
-console.log(`hello ${myfriends[2]}`);
-console.log(`${myfriends[1][2]}`);
-console.log(`${myfriends[3][1][2]}`);
+// console.log(`hello ${myfriends[0]}`);
+// console.log(`hello ${myfriends[2]}`);
+// console.log(`${myfriends[1][2]}`);
+// console.log(`${myfriends[3][1][2]}`);
 
-console.log(myfriends);
-myfriends[1] = "gamal";
-console.log(myfriends);
-myfriends[3] = ["sameh", "ameer"];
-console.log(myfriends);
+// console.log(myfriends);
+// myfriends[1] = "gamal";
+// console.log(myfriends);
+// myfriends[3] = ["sameh", "ameer"];
+// console.log(myfriends);
 
-console.log(Array.isArray(myfriends));
+// console.log(Array.isArray(myfriends));
+
+let myfriends = ["ahmed", "mohamed", "sayed", "alaa"];
+console.log(myfriends.length);
+myfriends[myfriends.length - 1] = "faysal";
+console.log(myfriends);
