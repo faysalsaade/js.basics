d9ce1d204142641774adef30d2215d3913cefbdb
faysalsaade <faysalsaadeh@gmail.com>
2024-07-04 21:42:25 +0300

    create a new project

--- from_48_to53/index8.js
+++ from_48_to53/index8.js
@@ -10,19 +10,31 @@
 // }
 // console.log(onlynames);
 
+// let products = ["keyboard", "mouse", "pen", "pad", "monitor"];
+// let colors = ["red", "green", "black"];
+// let models = [2020, 2021];
+// for (let i = 0; i < products.length; i++) {
+//   console.log("#".repeat(15));
+//   console.log(`# ${products[i]}`);
+//   console.log("#".repeat(15));
+//   console.log(`${"colors:"}`);
+//   for (let j = 0; j < colors.length; j++) {
+//     console.log(`- ${colors[j]}`);
+//   }
+//   console.log(`${"models:"}`);
+//   for (let k = 0; k < models.length; k++) {
+//     console.log(`- ${models[k]}`);
+//   }
+// }
 let products = ["keyboard", "mouse", "pen", "pad", "monitor"];
 let colors = ["red", "green", "black"];
-let models = [2020, 2021];
-for (let i = 0; i < products.length; i++) {
-  console.log("#".repeat(15));
-  console.log(`# ${products[i]}`);
-  console.log("#".repeat(15));
-  console.log(`${"colors:"}`);
-  for (let j = 0; j < colors.length; j++) {
+
+mainloop: for (let i = 0; i < products.length; i++) {
+  console.log(products[i]);
+  nestedloop: for (let j = 0; j < products.length; j++) {
     console.log(`- ${colors[j]}`);
-  }
-  console.log(`${"models:"}`);
-  for (let k = 0; k < models.length; k++) {
-    console.log(`- ${models[k]}`);
+    if (colors[j] === "green") {
+      break mainloop;
+    }
   }
 }
