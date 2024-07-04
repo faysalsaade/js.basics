f0b0dd79c7fc4e38ca099c1691f8bb16059b1ece
faysalsaade <faysalsaadeh@gmail.com>
2024-07-04 21:19:09 +0300

    create a new project

--- from_48_to53/index8.js
+++ from_48_to53/index8.js
@@ -1,3 +1,28 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+// for (let i = 0; i < 10; i = i + 2) {
+//   console.log(i);
+// }
+// let myfriends = ["fifi", "fofo", "fafi", "foufou", "fafa"];
+// let onlynames = [];
+// for (let i = 0; i < myfriends.length; i++) {
+//   if (typeof myfriends[i] === "string") {
+//     onlynames.push(myfriends[i]);
+//   }
+// }
+// console.log(onlynames);
+
+let products = ["keyboard", "mouse", "pen", "pad", "monitor"];
+let colors = ["red", "green", "black"];
+let models = [2020, 2021];
+for (let i = 0; i < products.length; i++) {
+  console.log("#".repeat(15));
+  console.log(`# ${products[i]}`);
+  console.log("#".repeat(15));
+  console.log(`${"colors:"}`);
+  for (let j = 0; j < colors.length; j++) {
+    console.log(`- ${colors[j]}`);
+  }
+  console.log(`${"models:"}`);
+  for (let k = 0; k < models.length; k++) {
+    console.log(`- ${models[k]}`);
+  }
 }
