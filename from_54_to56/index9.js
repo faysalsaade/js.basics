3761b530e6ea8ba39c273b7481b0b9c4f4e384d9
faysalsaade <faysalsaadeh@gmail.com>
2024-07-09 22:45:50 +0300

    create a new project

--- from_54_to56/index9.js
+++ from_54_to56/index9.js
@@ -4,3 +4,15 @@
 //   console.log(products[i]);
 //   i++;
 // }
+
+let products = ["keyboard", "mouse", "pen", "monitor", "iphone"];
+
+let i = 0;
+
+do {
+  console.log(products[i]);
+  i++;
+} while (false);
+{
+  console.log(i);
+}
