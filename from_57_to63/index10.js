b8bedd442ab550f5d6098d427705b9ad0c8b1d04
faysalsaade <faysalsaadeh@gmail.com>
2024-07-13 21:50:56 +0300

    create a new project

--- from_57_to63/index10.js
+++ from_57_to63/index10.js
@@ -1,7 +1,26 @@
-function sayhello(username) {
-  console.log(`hi ${username}`);
+// function sayhello(username) {
+//   console.log(`hi ${username}`);
+// }
+// sayhello("fifi");
+// sayhello("foufi");
+// sayhello("fafi");
+// sayhello("foufou");
+function sayhello(name, age) {
+  if (age < 20) {
+    console.log(`app is not suitable for you`);
+  } else {
+    console.log(`hello ${name} you age is ${age}`);
+  }
 }
-sayhello("fifi");
-sayhello("foufi");
-sayhello("fafi");
-sayhello("foufou");
+sayhello("fifi", 38);
+sayhello("fafi", 14);
+sayhello("foufou", 30);
+function generateyears(start, end, exclude) {
+  for (let i = start; i <= end; i++) {
+    if (i === exclude) {
+      continue;
+    }
+    console.log(i);
+  }
+}
+generateyears(2005, 2025, 2021);
