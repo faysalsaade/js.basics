7a41e897585f7d99ef510b1a3dd8bab69e72f328
faysalsaade <faysalsaadeh@gmail.com>
2024-07-18 21:26:29 +0300

    create a new project

---
+++ from_64_to70/index11.js
@@ -0,0 +1,12 @@
+let calculator = function (num1, num2) {
+  return num1 + num2;
+};
+console.log(calculator(10, 3));
+
+function sayhello() {
+  console.log("hello fifi");
+}
+document.getElementById("fafi").onclick = sayhello;
+setTimeout(function () {
+  console.log("the king faysal");
+}, 6000);
