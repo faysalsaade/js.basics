3cf698238f5367ff4a8da101b5ab78cab5370191
faysalsaade <faysalsaadeh@gmail.com>
2024-07-28 11:08:18 -0700

    create new project

---
+++ project/project.js
@@ -0,0 +1,53 @@
+
+// question 1
+
+//     let  num = Math.floor(Math.random() * 10) + 1;
+//    let  gues = parseInt(prompt("Game Faysal :Gues the number  between 1 and 10:"));
+//    if (gues === num) {
+//         alert("Congratulations king faysal! You guesed the correct number.");
+//    } else {
+//        alert(`Sorry faysal , the correct number was ${num} ,sorry omar!`);
+//    }
+
+
+// question 2
+// for (let index = 1; index < 11; index++) {
+//     console.log(index);
+    
+// }
+
+
+// question 3
+
+
+//  let print = function (num1,num2) {
+//    return num1 + num2;
+//  };
+// console.log(print(10,20));
+
+
+
+// question 4
+
+
+// let colors = ["red","yellow","black","white","green"]
+// for (let index = 0; index < colors.length; index++) {
+//     console.log(colors[index]);
+// }
+
+
+
+// // question 5
+
+
+// function printOddOrEven(number) {
+//     if (number % 2 === 0) {
+//         console.log(`${number} is even.`);
+//     } else {
+//         console.log(`${number} is odd.`);
+//     }
+// }
+
+// // Example usage:
+// printOddOrEven(4); // Output: "4 is even."
+// printOddOrEven(7); // Output: "7 is odd."
