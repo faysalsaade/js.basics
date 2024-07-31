9be63ec7659b173e7870ecd7d50c4a6fb3a1ea6a
faysalsaade <faysalsaadeh@gmail.com>
2024-07-31 12:36:09 -0700

    create new project

---
+++ from_79_to85/index13.js
@@ -0,0 +1,11 @@
+let user = {
+    thename: "faysal",
+    theage: 16,
+  
+    sayhello: function () {
+       return `hello`;
+    },
+};
+console.log(user.thename);
+console.log(user.theage);
+console.log(user.sayhello());
