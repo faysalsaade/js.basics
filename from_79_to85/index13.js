07685bf157193dc2e3c93ee7a3f9052736b9189e
faysalsaade <faysalsaadeh@gmail.com>
2024-08-01 12:25:32 -0700

    create new project

--- from_79_to85/index13.js
+++ from_79_to85/index13.js
@@ -10,14 +10,34 @@
 // console.log(user.theage);
 // console.log(user.sayhello());
 
-let myvar = "country";
+// let myvar = "country";
+// let user = {
+//     thename: "faysal",
+//     country: "france",
+//       // "country of": "lebanon",
+// };
+// console.log(user.country);
+// console.log(user.thename);
+// console.log(user.myvar);
+// console.log(user[myvar]);
+// // console.log(user["country of"]); 
 let user = {
-    thename: "faysal",
-    country: "france",
-      // "country of": "lebanon",
+name : "faysal",
+age : 16,
+skills : ["html","css","javascript"],
+available : false,
+addresses : {
+  ksa : "riyadh",
+  egypt: {
+    one : "cAIRO",
+    two : "giza",
+  },
+},
+checkav :function () {
+  if (available === true) {
+    return `free for work`;
+  }else{
+    return `not free`;
+  }
+}
 };
-console.log(user.country);
-console.log(user.thename);
-console.log(user.myvar);
-console.log(user[myvar]);
-// console.log(user["country of"]); 
