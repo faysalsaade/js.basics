6a8400d0c892d5b09c7af709ea7a59e383f0f2e2
faysalsaade <faysalsaadeh@gmail.com>
2024-08-04 02:05:20 -0700

    create new project

--- from_79_to85/index13.js
+++ from_79_to85/index13.js
@@ -220,22 +220,20 @@
 // console.log(objMethodFour.property()); // "Method Four"
 
 
-let a = 1;
+// let a = 1;
 
-let threeNums = {
-  b: 2,
-  c: 3,
-  d: 4,
-};
+// let threeNums = {
+//   b: 2,
+//   c: 3,
+//   d: 4,
+// };
 
-let twoNums = {
-  e: 5,
-  f: 6,
-};
-
-// Create Your Object Here in One Line
-
-console.log(finalObject);
+// let twoNums = {
+//   e: 5,
+//   f: 6,
+// };
+//   let finalObject  = Object.assign({a : 1},threeNums,twoNums)
+// console.log(finalObject);
 
 /*
   a: 1
