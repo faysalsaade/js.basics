55d95d635f78ef2af46967ba7d7a7b142f4a5d22
faysalsaade <faysalsaadeh@gmail.com>
2024-08-06 13:17:52 -0700

    create new project

---
+++ from_86_to93/index14.js
@@ -0,0 +1,21 @@
+// let myelement = document.createElement("div") ;
+// let myattr = document.createAttribute ("data-custom"    )
+// let mytext = document.createTextNode("fifi")
+// let mycomment = document.createComment("this is div")
+
+
+// myelement.className = "product";
+// myelement.setAttributeNode(myattr);
+// myelement.setAttribute("data-test","testing")  
+
+
+// //append comment to element 
+// myelement.appendChild(mycomment);
+
+
+// //append text to element
+// myelement.appendChild(mytext)
+
+
+// //append element to body 
+// document.body.appendChild(myelement)
