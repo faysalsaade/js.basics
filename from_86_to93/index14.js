8665f1b6c6d9c6b14b52c6003fbd1a70f13f96e7
faysalsaade <faysalsaadeh@gmail.com>
2024-08-10 12:42:36 -0700

    create new project

--- from_86_to93/index14.js
+++ from_86_to93/index14.js
@@ -57,3 +57,23 @@
 // window.onscroll = function () {
 //     console.log("scroll");
 // }
+let userinput = document.querySelector('[name = "username"]');
+let ageinput = document.querySelector("[name ='age']");
+
+document.forms[0].onsubmit = function (e) {
+    let uservalid = false ; 
+    let agevalid = false ;
+
+    if (userinput.value !== "" && userinput.value.length <= 10) {
+        uservalid = true;
+    }
+
+   if (ageinput.value !== "") {
+    agevalid = true;
+}
+
+ if (uservalid === false || agevalid === false) {
+    e.preventDefault();
+ }
+}; 
+
