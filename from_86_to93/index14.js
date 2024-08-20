71140041304c06af90cf2ec2b3a5b893617fc921
faysalsaade <faysalsaadeh@gmail.com>
2024-08-20 11:45:23 -0700

    create new project

--- from_86_to93/index14.js
+++ from_86_to93/index14.js
@@ -142,4 +142,4 @@
     // }
 let fifi= document.getElementsByTagName("input").value
 console.log(fifi);
-let cre = document.getElementsByName("create")[0];
+let cre = document.getElementsByName("create")[0].onclick.value;
