af28ed525eeef103af86cbbaec87592b988c4c7f
faysalsaade <faysalsaadeh@gmail.com>
2024-08-21 13:32:03 -0700

    create new project

--- from_86_to93/index14.js
+++ from_86_to93/index14.js
@@ -143,17 +143,14 @@
 function myfunction(e) {
     let fofo = document.getElementsByName("texts")[0].value
 let ff = `<div class = "sis">${fofo}</div>`
-document.getElementsByClassName("results")[0].innerHTML = ff
+
 
 let fofi = document.getElementsByName("elements")[0].value
-let ffi = fofi
-document.getElementsByClassName("results")[0].innerHTML = ffi
-for (let index = 0; index < ffi ; index++) {
-    let ffi = `<div class = "sis">${ffi}</div>`
-    document.getElementsByClassName("results")[0].innerHTML = ffi
+for (let index = 0; index < fofi; index++) {
+    document.getElementsByClassName("results")[0].innerHTML = ff
 }
-console.log("ffff");
-
     return false;
 }
+
+
 
