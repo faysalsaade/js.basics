187541523dfad45a62aa69c8f96f1e0a3d008571
faysalsaade <faysalsaadeh@gmail.com>
2024-08-26 11:18:04 -0700

    create new project

--- from_94_to101/index15.js
+++ from_94_to101/index15.js
@@ -7,3 +7,14 @@
 // one.onblur = function () {
 //     document.links[0].click()
 // }
+
+
+let element = document.getElementById("my-div")
+console.log(element.classList);
+console.log(typeof element.classList);
+
+console.log(element.classList.length);
+
+console.log(element.classList.contains("fofoi"));
+console.log(element.classList.contains("show"));
+
