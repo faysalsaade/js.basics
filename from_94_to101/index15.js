373d966e2255ec4b7fe3e025725a8cd6ed386121
faysalsaade <faysalsaadeh@gmail.com>
2024-08-26 12:59:48 -0700

    create new project

--- from_94_to101/index15.js
+++ from_94_to101/index15.js
@@ -57,9 +57,9 @@
 //     span.parentElement.remove()
 // }
 
-let myp = document.querySelector("p").cloneNode(true)
-let mydiv = document.querySelector("div")
+// let myp = document.querySelector("p").cloneNode(true)
+// let mydiv = document.querySelector("div")
 
-myp.id = `${myp.id}-clone`
+// myp.id = `${myp.id}-clone`
 
-mydiv.appendChild(myp)
+// mydiv.appendChild(myp)
