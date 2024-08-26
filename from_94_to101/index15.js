6a5d2b5ef7bb03f89428f6f8d0eff8620d0042c8
faysalsaade <faysalsaadeh@gmail.com>
2024-08-26 13:05:10 -0700

    create new project

--- from_94_to101/index15.js
+++ from_94_to101/index15.js
@@ -57,9 +57,24 @@
 //     span.parentElement.remove()
 // }
 
+
 // let myp = document.querySelector("p").cloneNode(true)
 // let mydiv = document.querySelector("div")
 
 // myp.id = `${myp.id}-clone`
 
 // mydiv.appendChild(myp)
+
+
+let myp = document.querySelector("P")
+
+myp.onclick = one ;
+myp.onclick = two ;
+function one() {
+    console.log("message from onclick 1");
+    
+}
+function two() {
+    console.log("message from onclick 2");
+    
+}
