6392fb92ae642a86d3b14f505f5462c2c149fabf
faysalsaade <faysalsaadeh@gmail.com>
2024-08-26 11:12:33 -0700

    create new project

--- from_94_to101/index15.js
+++ from_94_to101/index15.js
@@ -1,9 +1,9 @@
-let one = document.querySelector(".one")
-let two = document.querySelector(".two")
+// let one = document.querySelector(".one")
+// let two = document.querySelector(".two")
 
-window.onload = function () {
-    two.focus();
-}
-one.onblur = function () {
-    document.links[0].click()
-}
+// window.onload = function () {
+//     two.focus();
+// }
+// one.onblur = function () {
+//     document.links[0].click()
+// }
