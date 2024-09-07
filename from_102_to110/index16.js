028914652ea192f640bfa6ba43f14633d1bbc8cf
faysalsaade <faysalsaadeh@gmail.com>
2024-09-07 12:01:07 -0700

    create new project

--- from_102_to110/index16.js
+++ from_102_to110/index16.js
@@ -6,10 +6,5 @@
 // } else {
 //   console.log("you are donkey");
 // }
-let confirmMsg = confirm("do you love fifi");
-console.log(confirmMsg);
-if (confirmMsg === true) {
-  console.log("thank you");
-} else {
-  console.log("you are donkey");
-}
+let promptMsg = prompt("do you love fifi");
+console.log(promptMsg);
