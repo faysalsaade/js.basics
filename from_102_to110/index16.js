493e0a53fbaa56b608eac4dc891bc2c66245456a
faysalsaade <faysalsaadeh@gmail.com>
2024-09-07 12:34:36 -0700

    create new project

--- from_102_to110/index16.js
+++ from_102_to110/index16.js
@@ -14,7 +14,11 @@
 //   console.log(`i am msg from (${user})   his age is : ${age}`);
 // }
 
-setTimeout(saymsg, 2000, "fifi", 16);
-function saymsg(user, age) {
-  console.log(`i am msg from (${user})   his age is : ${age}`);
+let counter = setTimeout(saymsg, 4000);
+function saymsg() {
+  console.log(`i am msg `);
 }
+let btn = document.querySelector("button");
+btn.onclick = function () {
+  clearTimeout(counter);
+};
