9ea74df186f0db6e130fd25620daf72bdbde0ec0
faysalsaade <faysalsaadeh@gmail.com>
2024-10-13 13:15:09 -0700

    create new project

--- index17/index17.js
+++ index17/index17.js
@@ -328,3 +328,24 @@
 //   console.log(prop, myObject[prop]);
 // }
 // console.log(myObject);
+const myObject = {
+  a: 1,
+  b: 2,
+};
+
+Object.defineProperties(myObject, {
+  c: {
+    configurable: true,
+    value: 3,
+  },
+  d: {
+    configurable: true,
+    value: 4,
+  },
+  e: {
+    configurable: true,
+    value: 5,
+  },
+});
+console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
+console.log(Object.getOwnPropertyDescriptors(myObject));
