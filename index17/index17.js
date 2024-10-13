1c2cc8fb4c30a5146bee84c49b031f430a7a6d05
faysalsaade <faysalsaadeh@gmail.com>
2024-10-13 12:53:46 -0700

    create new project

--- index17/index17.js
+++ index17/index17.js
@@ -305,3 +305,19 @@
 //   return `.${this}.`;
 // };
 // let mystring = "Elzero";
+
+const myObject = {
+  a: 1,
+  b: 2,
+};
+Object.defineProperty(myObject, "c", {
+  writable: false,
+  enumerable: true,
+  configurable: true,
+  value: 3,
+});
+myObject.c = 100;
+for (let prop in myObject) {
+  console.log(prop, myObject[prop]);
+}
+console.log(myObject);
