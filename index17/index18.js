a3ecd8813d3ed87495f1c292457607c203622cb5
faysalsaade <faysalsaadeh@gmail.com>
2024-10-25 12:20:56 -0700

    create new project

--- index17/index18.js
+++ index17/index18.js
@@ -3,8 +3,12 @@
 // console.log(arr);
 // console.log(s());
 
-import fifi, { a, arr, saysomething as s } from "./index17.js";
-console.log(a);
-console.log(arr);
-console.log(s());
-console.log(fifi());
+// import fifi, { a, arr, saysomething as s } from "./index17.js";
+// console.log(a);
+// console.log(arr);
+// console.log(s());
+// console.log(fifi());
+
+import * as all from "./index17.js";
+console.log(all);
+console.log(all.arr);
