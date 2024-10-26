59f30f30a5d7b4b951cfbca26fa54449be15a31a
faysalsaade <faysalsaadeh@gmail.com>
2024-10-26 08:58:16 -0700

    create new project

--- index17/index18.js
+++ index17/index18.js
@@ -12,3 +12,18 @@
 // import * as all from "./index17.js";
 // console.log(all);
 // console.log(all.arr);
+
+const myJsonObjectFromServer = '{"Username":"faysal","Age":16}';
+console.log(typeof myJsonObjectFromServer);
+console.log(myJsonObjectFromServer);
+
+const myJsonObject = JSON.parse(myJsonObjectFromServer);
+console.log(typeof myJsonObject);
+console.log(myJsonObject);
+
+myJsonObject["Username"] = "Elzero";
+myJsonObject["Age"] = 40;
+
+const myJsonObjecttoServer = JSON.stringify(myJsonObject);
+console.log(typeof myJsonObjecttoServer);
+console.log(myJsonObjecttoServer);
