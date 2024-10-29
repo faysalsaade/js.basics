342842644614da68958b7692183d33d2d8dc04fa
faysalsaade <faysalsaadeh@gmail.com>
2024-10-29 16:58:31 +0200

    create new project

--- index17/index18.js
+++ index17/index18.js
@@ -89,10 +89,10 @@ let myRequest = new XMLHttpRequest();
 myRequest.open("GET", "https://api.github.com/users/elzerowebschool/repos");
 myRequest.send();
 console.log(myRequest);
-myRequest.onreadystatechange = function () {
-  //   console.log(myRequest.readyState);
-  //   console.log(myRequest.status);
-  if (this.readyState === 4 && this.status === 200) {
-    console.log(this.responseText);
-  }
-};
+// myRequest.onreadystatechange = function () {
+//   //   console.log(myRequest.readyState);
+//   //   console.log(myRequest.status);
+//   if (this.readyState === 4 && this.status === 200) {
+//     console.log(this.responseText);
+//   }
+// };
