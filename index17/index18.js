5d5483a26fa09378b089cee5cb738a46744e64fa
faysalsaade <faysalsaadeh@gmail.com>
2024-11-02 21:27:39 +0200

    create new project

--- index17/index18.js
+++ index17/index18.js
@@ -217,13 +217,23 @@
 //         document.body.appendChild(div);
 //       }
 //     }
+const getData = (apilink) => {
+  return new Promise((resolve, reject) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      } else {
+        reject(Error("no data found"));
+      }
+    };
 
-  let myRequest = new XMLHttpRequest();
-  myRequest.onreadystatechange = function () {
-    if ((this.readyState === 4) & (this.status === 200)) {
-      let jsData = JSON.parse(this.responseText);
-      for (let index = 0; index < jsData.length; index++) {
-        let div = document.createElement("div");
-        let repoName = document.createTextNode(jsData[index].name);
-        div.appendChild(repoName);
-        document.body.appendChild(div);
+    myRequest.open("GET", apilink);
+    myRequest.send();
+  });
+};
+
+getData("https://api.github.com/users/elzerowebschool/repos").then((result) => {
+  result.length = 10;
+  return result;
+});
