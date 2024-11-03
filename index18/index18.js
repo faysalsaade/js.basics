55681528bb1fa8620f19c106b36e6f114a86df97
faysalsaade <faysalsaadeh@gmail.com>
2024-11-03 12:26:12 +0200

    create new project

--- index18/index18.js
+++ index18/index18.js
@@ -1,1 +1,12 @@
-ddddddddddddddddddddddddddddddddddddddddddddd;
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("i am the good promise");
+  }, 3000);
+});
+async function readData() {
+  console.log("before promise");
+  //   myPromise.then((resolveValue) => console.log(resolveValue));
+  console.log(await myPromise);
+  console.log("after promise");
+}
+readData();
