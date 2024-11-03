ffa279f56c3e4c3c564751188ca748edfd45088c
faysalsaade <faysalsaadeh@gmail.com>
2024-11-03 13:16:46 +0200

    create new project

--- index18/index18.js
+++ index18/index18.js
@@ -1,12 +1,25 @@
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("i am the good promise");
-  }, 3000);
-});
-async function readData() {
-  console.log("before promise");
-  //   myPromise.then((resolveValue) => console.log(resolveValue));
-  console.log(await myPromise);
-  console.log("after promise");
-}
-readData();
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject(Error("i am the bad promise"));
+//   }, 3000);
+// });
+// async function readData() {
+//   console.log("before promise");
+//   //   myPromise.then((resolveValue) => console.log(resolveValue));
+//   console.log(await myPromise.catch((err) => err));
+//   console.log("after promise");
+// }
+// readData();
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("i am the bad promise");
+//   }, 3000);
+// });
+// async function readData() {
+//   console.log("before promise");
+//   //   myPromise.then((resolveValue) => console.log(resolveValue));
+//   console.log(await myPromise);
+//   console.log("after promise");
+// }
+// readData();
