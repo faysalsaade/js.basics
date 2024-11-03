047aad1546c995b170e2c81a5812f45e0f62ed4b
faysalsaade <faysalsaadeh@gmail.com>
2024-11-03 14:41:06 +0200

    create new project

--- index20/index20.js
+++ index20/index20.js
@@ -1,16 +1,19 @@
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("i am the good promise ");
-  }, 3000);
-});
-async function fetchData() {
-  console.log("before promise");
-  try {
-    console.log(await myPromise);
-  } catch (reason) {
-    console.log(`reason: ${reason}`);
-  } finally {
-    console.log("After promise");
-  }
-}
-fetchData();
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("i am the good fetch ");
+//   }, 3000);
+// });
+// async function fetchData() {
+//   console.log("before fetch");
+//   try {
+//     let mydata = await fetch(
+//       "https://api.github.com/users/elzerowebschool/repos"
+//     );
+//     console.log(await mydata.json());
+//   } catch (reason) {
+//     console.log(`reason: ${reason}`);
+//   } finally {
+//     console.log("After fetch");
+//   }
+// }
+// fetchData();
